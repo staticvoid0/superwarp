fabc904fd03fae68ca579cecdc359438faf38463
AkadenTK <akaden.ffxi@gmail.com>
2026-05-17 11:53:23 -0500

    Update special thanks
    Thanks Staticvoid!

--- superwarp.lua
+++ superwarp.lua
@@ -39,6 +39,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         Sortie  support implementation: Staticvoid
         Odyssey support implementation: Staticvoid
         Apollyon  and  Temenos support: Staticvoid
+		Mog Garden support            : Staticvoid
+        Maintenance			          : Staticvoid
 ]]
 
 _addon.name = 'superwarp'
